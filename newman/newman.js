78fdbcc3d8cd6151944d1d56fe8298055a34e7d4
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-04 17:49:27 +0800

    Update newman.js

--- newman/newman.js
+++ newman/newman.js
@@ -10,26 +10,4 @@ newman.run({
     }
 })
 
-import { DynamoDBClient, DescribeTableCommand } from "@aws-sdk/client-dynamodb";
-
-const client = new DynamoDBClient({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
-});
-
-const command = new DescribeTableCommand({
-  TableName: 't-info' // 替换为你的表格名称
-});
-
-client.send(command)
-  .then(data => {
-    console.log('Success! Table description:', data.Table);
-  })
-  .catch(err => {
-    console.error('Error:', err);
-  });
-
 
