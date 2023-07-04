c7c0c3d1addbfae5af43cec608c2318c75efcece
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-04 18:00:36 +0800

    Update DynamoDb.js

--- newman/DynamoDb.js
+++ newman/DynamoDb.js
@@ -1,4 +1,4 @@
-const { DynamoDBClient, DescribeTableCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, DescribeTableCommand ,QueryCommand} = require("@aws-sdk/client-dynamodb");
 
 const client = new DynamoDBClient({
   region: process.env.AWS_REGION,
@@ -19,3 +19,21 @@ client.send(command)
   .catch(err => {
     console.error('Error:', err);
   });
+
+const params = {
+  TableName: 't-InfoLog', // 替换为你的表格名称
+  KeyConditionExpression: 'serviceId = :value', // 设置查询条件表达式，不再使用占位符
+  ExpressionAttributeValues: {
+    ':value': { S: '01c874e1-54a6-4991-b582-a1960a80fd55' } // 替换为你的查询值和数据类型
+  }
+};
+
+const command = new QueryCommand(params);
+
+try {
+  const response = await client.send(command);
+  console.log('Success! Query results:', response.Items);
+} catch (error) {
+  console.error('Error:', error);
+}
+
