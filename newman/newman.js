e59aed52f19d20d20cb00ab9e713048d3b171c12
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-04 17:10:42 +0800

    Update newman.js

--- newman/newman.js
+++ newman/newman.js
@@ -9,3 +9,25 @@ newman.run({
         console.log(res.response.code)
     }
 })
+
+// 引入 AWS SDK
+const AWS = require('aws-sdk');
+
+// 配置 AWS 凭证和区域
+AWS.config.update({
+  accessKeyId: 'AKIARA2JM4WX3VI6HL4T',
+  secretAccessKey: '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',
+  region: 'ap-south-1' // 替换为你的区域
+});
+
+// 创建 DynamoDB 客户端
+const dynamodb = new AWS.DynamoDB();
+
+// 示例：获取 DynamoDB 表的信息
+dynamodb.describeTable({ TableName: 't-info' }, (err, data) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Table Information:', data.Table);
+  }
+});
