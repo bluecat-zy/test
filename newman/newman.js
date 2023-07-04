7ff87e09809115337ed3f6c4fca28853b2963208
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-04 16:34:44 +0800

    Add files via upload

--- newman/newman.js
+++ newman/newman.js
@@ -1,1 +1,11 @@
+const newman = require('newman');
 
+newman.run({
+    collection: require('./20230629FXS.postman_collection.json')
+
+}).on('done', function (err, response) {
+    for (let res of response.run.executions) {
+        console.log(res.response.status)
+        console.log(res.response.code)
+    }
+})
