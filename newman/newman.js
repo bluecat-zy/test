0fd352bcb7dfa983458ba07dc2019b89231758bc
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-05 14:31:01 +0800

    Update newman.js

--- newman/newman.js
+++ newman/newman.js
@@ -1,48 +1,52 @@
-//const newman = require('newman');
-var data1 ='123';
-data1 ='234';
-module.exports.data1 = data1;
-//const data =[];
-// newman.run({
-//     collection: require('./20230629FXS.postman_collection.json')
-// }).on('beforeRequest', (error, args) => {
-//       const requestBodyRaw = args.request.body.raw;
-        
-//         try {
-//             const requestBodyArray = JSON.parse(`[${requestBodyRaw}]`);
-//             requestBodyArray.forEach((requestBody, index) => {
-//                 if (require.main === module) {
-//                 console.log(`Request ${index + 1} Parameters:`);
-//                 }
-//                 Object.entries(requestBody).forEach(([key, value]) => {
-//                      if (require.main === module) {
-//                     console.log(`${key}: ${value}`);
-//                      }
-//                 });
-//                 if (require.main === module) {
-//                 console.log('\n');
-//                 }
-//             });
-//         } catch (e) {
-//             if (require.main === module) {
-//             console.error('Error parsing request body:', e);
-//         }
-//      }
-// }).on('done', function (err, response) {
-//     for (let res of response.run.executions) {
-//         const responseTimeHeader = res.response.headers.find(header => header.key.toLowerCase() === 'date');
-//          if (require.main === module) {
-//         console.log(res.response.status)
-//         console.log(res.response.code)
-//          }
-//         const date = new Date(responseTimeHeader).toISOString().replace('.000Z', '');     
-//         data.push({status:res.response.status,code:res.response.code,date:date });
-//     }
-//     module.exports.data = data;
-//     if (require.main === module) {
-//    console.log(module.exports.data) 
-//     }
-// })
+const newman = require('newman');
+var data =[];
+var requestBodyRaw =[];
+module.exports.data = data;
+newman.run({
+    collection: require('./20230629FXS.postman_collection.json')
+}).on('beforeRequest', (error, args) => {
+       requestBodyRaw = args.request.body.raw;
+}).on('done', function (err, response) {
+         try {
+            const requestBodyArray = JSON.parse(requestBodyRaw);
+         if (Array.isArray(requestBodyArray)) {
+             jsonDataArray.forEach((data) => {
+             const vin = findVinValue(data);
+             const data = {vin:'',timestamp:''};
+             data.vin = vin;
+             dataArray.push(data);
+            });
+         } else {
+               
+         }
+        } catch (e) {
+          if (require.main === module) {
+            console.error('Error parsing request body:', e);
+          }
+     }
+    for (let res of response.run.executions) {
+        const responseTimeHeader = res.response.headers.find(header => header.key.toLowerCase() === 'date');
+         if (require.main === module) {
+        console.log(res.response.status)
+        console.log(res.response.code)
+         }
+        const date = new Date(responseTimeHeader).toISOString().replace('.000Z', '');     
+        data.push({status:res.response.status,code:res.response.code,date:date });
+    }
+})
+
+const findVinValue = (obj) => {
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') { // 判断属性值是否为对象
+      const result = findVinValue(obj[key]); // 递归调用函数
+      if (result) {
+        return result; // 返回找到的结果
+      }
+    } else if (key === 'vin') {
+      return obj[key]; // 找到 vin 值，返回该值
+    }
+  }
+};
 
 
 
