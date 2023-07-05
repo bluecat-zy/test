bdab0c48030db1e6b0d88d247241528f5c85aa0f
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-05 13:58:33 +0800

    Update newman.js

--- newman/newman.js
+++ newman/newman.js
@@ -1,47 +1,47 @@
-const newman = require('newman');
+//const newman = require('newman');
 const data1 ='123';
 module.exports.data1 = data1;
-const data =[];
-newman.run({
-    collection: require('./20230629FXS.postman_collection.json')
-}).on('beforeRequest', (error, args) => {
-      const requestBodyRaw = args.request.body.raw;
+//const data =[];
+// newman.run({
+//     collection: require('./20230629FXS.postman_collection.json')
+// }).on('beforeRequest', (error, args) => {
+//       const requestBodyRaw = args.request.body.raw;
         
-        try {
-            const requestBodyArray = JSON.parse(`[${requestBodyRaw}]`);
-            requestBodyArray.forEach((requestBody, index) => {
-                if (require.main === module) {
-                console.log(`Request ${index + 1} Parameters:`);
-                }
-                Object.entries(requestBody).forEach(([key, value]) => {
-                     if (require.main === module) {
-                    console.log(`${key}: ${value}`);
-                     }
-                });
-                if (require.main === module) {
-                console.log('\n');
-                }
-            });
-        } catch (e) {
-            if (require.main === module) {
-            console.error('Error parsing request body:', e);
-        }
-     }
-}).on('done', function (err, response) {
-    for (let res of response.run.executions) {
-        const responseTimeHeader = res.response.headers.find(header => header.key.toLowerCase() === 'date');
-         if (require.main === module) {
-        console.log(res.response.status)
-        console.log(res.response.code)
-         }
-        const date = new Date(responseTimeHeader).toISOString().replace('.000Z', '');     
-        data.push({status:res.response.status,code:res.response.code,date:date });
-    }
-    module.exports.data = data;
-    if (require.main === module) {
-   console.log(module.exports.data) 
-    }
-})
+//         try {
+//             const requestBodyArray = JSON.parse(`[${requestBodyRaw}]`);
+//             requestBodyArray.forEach((requestBody, index) => {
+//                 if (require.main === module) {
+//                 console.log(`Request ${index + 1} Parameters:`);
+//                 }
+//                 Object.entries(requestBody).forEach(([key, value]) => {
+//                      if (require.main === module) {
+//                     console.log(`${key}: ${value}`);
+//                      }
+//                 });
+//                 if (require.main === module) {
+//                 console.log('\n');
+//                 }
+//             });
+//         } catch (e) {
+//             if (require.main === module) {
+//             console.error('Error parsing request body:', e);
+//         }
+//      }
+// }).on('done', function (err, response) {
+//     for (let res of response.run.executions) {
+//         const responseTimeHeader = res.response.headers.find(header => header.key.toLowerCase() === 'date');
+//          if (require.main === module) {
+//         console.log(res.response.status)
+//         console.log(res.response.code)
+//          }
+//         const date = new Date(responseTimeHeader).toISOString().replace('.000Z', '');     
+//         data.push({status:res.response.status,code:res.response.code,date:date });
+//     }
+//     module.exports.data = data;
+//     if (require.main === module) {
+//    console.log(module.exports.data) 
+//     }
+// })
 
 
 
