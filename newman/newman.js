687ced35a58053acaf803e5b1180c7e82688a6e9
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-05 10:10:26 +0800

    Update newman.js

--- newman/newman.js
+++ newman/newman.js
@@ -3,6 +3,7 @@ const newman = require('newman');
 newman.run({
     collection: require('./20230629FXS.postman_collection.json')
 }).on('beforeRequest', (error, args) => {
+     console.log('Request Parameters:', args.request.body.raw);
        const requestBody = JSON.parse(args.request.body.raw);
         
         Object.entries(requestBody).forEach(([key, value]) => {
