c1206265a04335df5851e5f28fe398d617ae8078
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-05 14:53:58 +0800

    Update newman.js

--- newman/newman.js
+++ newman/newman.js
@@ -1,5 +1,13 @@
 const newman = require('newman');
 const { DynamoDBClient, DescribeTableCommand ,QueryCommand} = require("@aws-sdk/client-dynamodb");
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
+
 var dataArray =[];
 var requestBodyRaw =[];
 
@@ -29,37 +37,25 @@ newman.run({
         const responseTimeHeader = res.response.headers.find(header => header.key.toLowerCase() === 'date');
         console.log(res.response.status)
         console.log(res.response.code)
-        const date = new Date(responseTimeHeader).toISOString().replace('.000Z', '');     
-       dataArray.push({status:res.response.status,code:res.response.code,date:date });
+        const date = new Date(responseTimeHeader).toISOString().replace('.000Z', ''); 
+        
+        const command = new QueryCommand(params);
+        const params = {
+        TableName: 't-InfoLog', // 表名
+        KeyConditionExpression: 'timestamp >= :date',
+        ExpressionAttributeValues: {
+        ':date': { S:date }
+        }
+        };
+        client.send(command)
+        .then((response) => {
+         console.log('Success! Query results:', response.Items);
+        })
+        .catch((error) => {
+        console.error('Error:', error);
+        });
     }
-    console.log(dataArray);
 })
-
-const client = new DynamoDBClient({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
-});
-
-const params = {
-  TableName: 't-InfoLog', // 表名
-  KeyConditionExpression: 'serviceId = :value', // 查询条件
-  ExpressionAttributeValues: {
-    ':value': { S: '01c874e1-54a6-4991-b582-a1960a80fd55' } // 查询值和数据类型
-  }
-};
-
-const command = new QueryCommand(params);
-
-client.send(command)
-  .then((response) => {
-    console.log('Success! Query results:', response.Items);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
 
 const findVinValue = (obj) => {
   for (let key in obj) {
