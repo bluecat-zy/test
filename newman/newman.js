529af71250eed08fa01c6d5a33e205d5b928fffe
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-06 17:38:22 +0800

    Update newman.js

--- newman/newman.js
+++ newman/newman.js
@@ -20,7 +20,7 @@ newman.run({
   } else {
     console.log('测试运行完成.');
     console.log("校验请求的执行结果需要等待DynamoDb同步数据---");
-    sleep(1).then(() => {
+    await sleep(1).then(() => {
     console.log("等待结束--------");
     });
     // 遍历每个请求的执行结果
@@ -40,23 +40,20 @@ newman.run({
       var date = new Date(responseTimeHeader).toISOString();
       console.log(`请求结束的时间：${date}`);
       date = date.substring(0,date.lastIndexOf(".")-8);
-      const params = {
-        TableName: 't-InfoLog', // 表名
-        FilterExpression: 'contains(#ts,:value) AND (#log = :value2 or #log = :value3) AND #ifid = :value4',
-        ExpressionAttributeNames: {
-        '#ts': 'timestamp',
-        '#log': 'logLevel',
-        '#ifid': 'ifid'
-        },
-        ExpressionAttributeValues: {
-        ':value': { S:date },
-        ':value2': { S:'ERROR'},
-        ':value3': { S:'WARN'},
-        ':value4': { S:'IT303E'}
-        }
-      };
+      const params = generateParam()
       const url = requestUrl.substring(requestUrl.lastIndexOf("/test")+5);
       console.log(ifidMap.get(url));
+      Object.entries(ifidMap).forEach(([key, value]) => {
+       if (value.url.includes('(.+)')) {
+         var position = value.url.indexOf('(.+)');
+         console.log(`模式在位置 ${position} 处找到vin`);
+         const url2 = value.url.substring(position)
+         const url3 = url2.includes("/")?url2.substring(0,url2.indexOf("/")):url2
+        console.log(url3);
+      } else {
+        console.log("字符串中未找到指定模式");
+      }
+      });
       const ifid = ifidMap.get(url);
       if (ifidMap.get(url)) {
       console.log('ifid获取成功：',url);
@@ -69,19 +66,8 @@ newman.run({
       send(command,requestName);
       if(ifid.lambda){
         if(ifid.lambda.GetMskLambda == true){
-      params.FilterExpression = 'contains(#ts,:value) AND (#log = :value2 or #log = :value3) AND #lambda = :value4';
-      params.ExpressionAttributeNames = {
-        '#ts': 'timestamp',
-        '#log': 'logLevel',
-        '#lambda': 'lambdaName'
-        };
-      params.ExpressionAttributeValues = {
-        ':value': { S:date },
-        ':value2': { S:'ERROR'},
-        ':value3': { S:'WARN'},
-        ':value4': { S:'GetMskLambda'}
-        };
-        const getMskLambdaCommand = new ScanCommand(params);
+        const getMskLambdaParam = generateParam(date,null,null,'GetMskLambda')
+        const getMskLambdaCommand = new ScanCommand(getMskLambdaParam);
         console.log('验证GetMskLambda的数据');
         send(getMskLambdaCommand,requestName);  
        }
@@ -93,10 +79,23 @@ newman.run({
 
 function sleep(minutes) {
   const milliseconds = minutes * 60000; // 将分钟转换为毫秒
-
   return new Promise(resolve => {
     setTimeout(resolve, milliseconds);
   });
+}
+
+function getIfid(requestUrl) {
+  const url = requestUrl.substring(requestUrl.lastIndexOf("/test")+5);
+  Object.entries(ifidMap).forEach(([key, value]) => {
+  if (value.url.includes('(.+)')) {
+  var position = value.url.indexOf('(.+)');
+  console.log(`模式在位置 ${position} 处找到vin`);
+  const url2 = value.url.substring(position)
+  const url3 = url2.includes("/")?url2.substring(0,url2.indexOf("/")):url2
+  console.log(url3);
+  } else {
+    console.log("字符串中未找到指定模式");
+  }
 }
 
 function send(command,requestName) {
@@ -117,6 +116,53 @@ function send(command,requestName) {
       console.error('Error:', error);
     });
 }
+
+function getVin(param){
+   var position = param.indexOf('(.+)');
+   const str = param.substring(position)
+   const result = str.includes("/")?str.substring(0,str.indexOf("/")):str;
+   return  result;
+}
+
+function generateParam(date,ifid,vin,lambda){
+   const param = {
+        TableName: 't-InfoLog', // 表名
+        FilterExpression: 'contains(#ts,:value1) AND (#log = :value2 or #log = :value3)',
+        ExpressionAttributeNames: {
+        '#ts': 'timestamp',
+        '#log': 'logLevel'
+        },
+        ExpressionAttributeValues: {
+        ':value1': { S:date },
+        ':value2': { S:'ERROR'},
+        ':value3': { S:'WARN'}
+        }
+    };
+  if(ifid){
+    // 获取下一个可用的键
+    const nextKey = `value${Object.keys(param.ExpressionAttributeValues).length + 1}`;
+    param.FilterExpression += ' AND #ifid = :'+nextKey;
+    param.ExpressionAttributeNames['#ifid'] = 'ifid';
+    param.ExpressionAttributeValues[nextKey] = { S:ifid};  
+  }
+  if(vin){
+    // 获取下一个可用的键
+    const nextKey = `value${Object.keys(param.ExpressionAttributeValues).length + 1}`;
+    param.FilterExpression += ' AND #vin = :'+nextKey;
+    param.ExpressionAttributeNames['#vin'] = 'vin';
+    param.ExpressionAttributeValues[nextKey] = { S:vin};  
+  }
+  if(lambda){
+    // 获取下一个可用的键
+    const nextKey = `value${Object.keys(param.ExpressionAttributeValues).length + 1}`;
+    param.FilterExpression += ' AND #lambda = :'+nextKey;
+    param.ExpressionAttributeNames['#lambda'] = 'lambdaName';
+    param.ExpressionAttributeValues[nextKey] = { S:lambda};
+  }
+  return param;
+}
+
+
 
 
 
