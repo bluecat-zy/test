63f859c2be484882141714f02589e7b1efc24843
yu-zhao1314521 <59995649+yu-zhao1314521@users.noreply.github.com>
2023-07-07 10:07:23 +0800

    Update test.py

--- newman/test.py
+++ newman/test.py
@@ -10,14 +10,24 @@ session = boto3.Session(
 rest_api_id = '4qppov5b81'
 ip_address = '192.169.96.201'
 # 创建 API Gateway 客户端
-client = boto3.client('apigateway' , region_name= 'ap-south-1')
+client = boto3.client('apigateway', region_name='ap-south-1')
+# 指定要连接的 API 的 RestApiId
+api_id = '4qppov5b81'
 
-response = client.get_rest_api(
-    restApiId = rest_api_id
-)
+try:
+    # 调用客户端的 get_rest_api 方法，指定对应的 RestApiId
+    response = client.get_rest_api(restApiId=api_id)
 
-policy = response['policy']
-print(policy)
+    # 输出返回结果
+    print(response)
+
+except Exception as e:
+    # 输出异常信息
+    print('发生错误:', str(e))
+
+
+#policy = response['policy']
+#print(policy)
 # sta = policy['Statement']
 # print(sta)
 # 使用示例
